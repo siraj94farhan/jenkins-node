e17b5922f65e3415f9c346ea12aa78b2d480b7f2
Siraj Farhan <siraj.farhan12@gmail.com>
2017-04-10 12:53:58 +0530

    refactor: change name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Farhan' });
+  res.render('index', { title: 'Siraj' });
 });
 
 module.exports = router;
