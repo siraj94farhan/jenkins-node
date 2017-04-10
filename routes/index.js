47e0ac73f055051b0619b13ace1a113e26802321
Siraj Farhan <siraj.farhan12@gmail.com>
2017-04-10 15:29:26 +0530

    refactor: chnaged name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Siraj' });
+  res.render('index', { title: 'Farhan' });
 });
 
 module.exports = router;
